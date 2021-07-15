09403a52d43eb08b72e3002ed65fe4e50dbebf9d
Nicolas Hanout <nicolas.hanout@gmail.com>
2021-07-14 17:04:10 -0700

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module org.homi.plugins.ruleengine.specification {
+	requires org.homi.plugin.specification;
+	
+	exports org.homi.plugins.ruleengine.specification;
+	
+}
---
+++ src/main/java/org/homi/plugins/ruleengine/specification/RuleEngineSpec.java
@@ -0,0 +1,49 @@
+package org.homi.plugins.ruleengine.specification;
+
+import static org.homi.plugin.specification.Constraints.*;
+import static org.homi.plugin.specification.SpecificationHelper.defineType;
+import static org.homi.plugin.specification.SpecificationHelper.processType;
+import static org.homi.plugin.specification.SpecificationHelper.processTypes;
+
+import java.util.List;
+import java.util.Map;
+
+import org.homi.plugin.specification.ISpecification;
+import org.homi.plugin.specification.SpecificationID;
+import org.homi.plugin.specification.types.TypeDef;
+
+class Types {
+	public static TypeDef<?> RULE_TEXT= defineType(String.class, notNull(), minLength(1)); 
+	public static TypeDef<?> RULE_RESULT= defineType(Map.class, notNull()); 
+	public static TypeDef<?> RULE_ID= defineType(Integer.class, notNull()); 
+	public static TypeDef<?> SUCCESS= defineType(Boolean.class, notNull()); 
+}
+
+@SpecificationID(id="RuleEngineSpec")
+public enum RuleEngineSpec implements ISpecification {
+	ADD_RULE(Types.SUCCESS, Types.RULE_TEXT),
+	GET_RULES(Types.RULE_TEXT),
+	REMOVE_RULE(Types.SUCCESS, Types.RULE_ID);
+	
+	private List<TypeDef<?>> parameterTypes;
+	private TypeDef<?> returnType;
+	
+	RuleEngineSpec(Object returnType, Object...parameterTypes ) {
+		try {
+			this.returnType = processType(returnType);
+			this.parameterTypes = processTypes(parameterTypes);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public List<TypeDef<?>> getParameterTypes() {
+		return this.parameterTypes;
+	}
+	
+	@Override
+	public TypeDef<?> getReturnType() {
+		return this.returnType;
+	}
+}
